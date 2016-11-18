310220e8783988930e78881df2f796d1648e0d36
Srta-Iny <iny.mercado@gmail.com>
2016-11-18 18:40:17 -0300

    se agregan: html( codigo, confirm, perfil), js: modal, dropdown

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,4 +1,19 @@
- $(document).ready(function() {
-    $('select').material_select();
-  });
- $('select').material_select('destroy');
+// dropdown
+$(document).ready(function() {
+	$('select').material_select();
+});
+$('select').material_select('destroy');
+
+// toast 
+$(document).ready(function(){
+	$('.tooltipped').tooltip({delay: 50});
+});
+
+// modal
+$(document).ready(function(){
+// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+	$('.modal').modal();
+	$('#modal1').modal('open');
+});
+
+         
