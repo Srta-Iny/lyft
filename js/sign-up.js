170d67711be397b1ff0845a229972ebb3de4c195
Srta-Iny <iny.mercado@gmail.com>
2016-11-21 01:55:00 -0300

    se agregan cambios

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,13 +1,34 @@
-// dropdown
+
+// dropdown telefonp
 $(document).ready(function() {
 	$('select').material_select();
 });
 $('select').material_select('destroy');
-// modal
+
+
+// obter datos a guardar 
+var pais = $('#paises').val();
+var fono = $('#icon_telephone').val();
+// guardar datos del usuario 
+$(document).ready(function nuevoUser() {
+	$('#boton-tel').click(function(){
+		console.log(fono);
+		if( fono == null || fono.length == 0 || /^\s+$/.test(fono)){
+			var $toastContent = $('<span> Campo obligatorio, por favor ingresa tu telefono </span>');
+			Materialize.toast($toastContent, 3000);
+			return false;
+		}else if(telefono.length < 8){
+			var $toastContent = $('<span> Tu contraseña debe tener al menos 9 digitos </span>');
+			Materialize.toast($toastContent, 3000);
+			return false;
+		}
+	});
+});
+// modal codigo  
 $(document).ready(function(){
 // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 	$('.modal').modal();
-	$('#addCard').modal('open');
+	$('#modal1').modal('open');
 });
 
-         
+		 
