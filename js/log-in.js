40e72515178224121329ebbf4b2711b12acd890b
Srta-Iny <iny.mercado@gmail.com>
2016-11-27 02:59:01 -0300

    se agregan funciones al mapa(calcular viaje), funcion sumar propina, y menu desplegable en mapa

--- js/log-in.js
+++ js/log-in.js
@@ -8,9 +8,36 @@ $(document).ready(function nuevoUser() {
 			return false;
 		} return true;
 	});
+
+	$(".button-collapse").sideNav();
+
+	$('.button-collapse').sideNav({
+	    menuWidth: 300, // Default is 240
+	    edge: 'right', // Choose the horizontal origin
+	    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+	    draggable: true // Choose whether you can drag to open on touch screens
+    	}
+  	);
 });
+//menu collapse
+
+// PAYMENT
+// estrellitas
 $(document).ready(function estrellitas(){
 	$('.star').click(function(){
 		$(this).toggleClass("grey-text yellow-text");
 	});
+});
+// valor carrera
+$(document).ready(function calcularCarrera(){
+	var viaje = $('#viaje');
+	valor = parseInt(localStorage.getItem('valorCarrera'));
+	viaje.html('$ ' + valor);
+	// sumar propina
+	$('#propina li').click(function(){
+		var tip= $(this).data('value');
+		var suma = valor + tip;
+		viaje.html('$ ' + suma);
+	});
+
 });
--- js/map.js
+++ js/map.js
@@ -1,34 +1,183 @@
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 16
-  });
-  var infoWindow = new google.maps.InfoWindow({map: map});
+	// variables para obtener id de ubicacion
+	var origin_place_id = null;
+	var destination_place_id = null;
+	var destino = null;
 
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
+	var travel_mode = google.maps.TravelMode.WALKING;
+	var map = new google.maps.Map(document.getElementById('map'), {
+		mapTypeControl: false,
+		center: {lat: -33.8688, lng: 151.2195},
+		zoom: 15
+	});
+	var centro = null;
+	// Try HTML5 geolocation.
+	// geolocalizacion muestra ubicacion actual 
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			centro = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+			map.setCenter(centro);
+			var marker = new google.maps.Marker({
+			position: centro,
+			map: map,
+			title: 'Hello World!'
+			})
+		}, function() {
+			alert('error');
+		});
+	} else {
+		// Browser doesn't support Geolocation
+		alert("Browser doesn't support Geolocation");
+	}
+	
+	// Geocoding genera latitud y longitud 
+	var directionsService = new google.maps.DirectionsService;
+	var directionsDisplay = new google.maps.DirectionsRenderer;
+	directionsDisplay.setMap(map);
 
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
+	var destination_input = document.getElementById('destination-input');
+
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(destination_input);
+
+	var destination_autocomplete = new google.maps.places.Autocomplete(destination_input);
+	destination_autocomplete.bindTo('bounds', map);
+
+	// Sets a listener on a radio button to change the filter type on Places
+	// Autocomplete.
+	travel_mode = google.maps.TravelMode.DRIVING;
+
+	function expandViewportToFitPlace(map, place) {
+		if (place.geometry.viewport) {
+			map.fitBounds(place.geometry.viewport);
+		} else {
+			map.setCenter(place.geometry.location);
+			map.setZoom(17);
+		}
+	}
+	
+	function geocodeLatLng() {
+	var geocoder = new google.maps.Geocoder;
+	var latlng = {lat: parseFloat(centro.lat), lng: parseFloat(centro.lng)};
+	geocoder.geocode({'location': latlng}, function(results, status) {
+		if (status === google.maps.GeocoderStatus.OK) {
+			origin_place_id = results[0].place_id;
+		ruta();
+		} else {
+			return false;
+		}
+	});
+	// matriz de distancia se ingresan datos para calcular en el callback
+	var service = new google.maps.DistanceMatrixService();
+	service.getDistanceMatrix(
+	{
+		origins: [centro],
+		destinations: [destino],
+		travelMode: google.maps.TravelMode.DRIVING,
+		unitSystem: google.maps.UnitSystem.METRIC,
+		avoidHighways: true,
+		avoidTolls: true,
+	}, callback);
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
+	/*origin_autocomplete.addListener('place_changed', function() {
+		var place = origin_autocomplete.getPlace();
+		if (!place.geometry) {
+			window.alert("Autocomplete's returned place contains no geometry");
+			return;
+		}
+		expandViewportToFitPlace(map, place);
 
+		// If the place has a geometry, store its place ID and route if we have
+		// the other place ID
+		origin_place_id = place.place_id;
+		route(origin_place_id, destination_place_id, travel_mode,
+					directionsService, directionsDisplay);
+	});*/
+
+	// toma los datos del input y genera ruta de destino
+	function ruta(){
+		route(origin_place_id, destination_place_id, travel_mode,
+					directionsService, directionsDisplay);
+	}
+	// funcion que toma los datos del autocompletar
+	destination_autocomplete.addListener('place_changed', function() {
+		var place = destination_autocomplete.getPlace();
+		if (!place.geometry) {
+			window.alert("Autocomplete's returned place contains no geometry");
+			return;
+		}
+		expandViewportToFitPlace(map, place);
+
+		destino = {lat: parseFloat(place.geometry.location.lat()), lng: parseFloat(place.geometry.location.lng())};
+		
+		destination_place_id = place.place_id;
+
+
+		// If the place has a geometry, store its place ID and route if we have
+	geocodeLatLng();
+		// the other place ID
+		
+		
+	});
+	// calcular ruta
+	function route(origin_place_id, destination_place_id, travel_mode,
+								 directionsService, directionsDisplay) {
+		if (!origin_place_id || !destination_place_id) {
+			return;
+		}
+		directionsService.route({
+			origin: {'placeId': origin_place_id},
+			destination: {'placeId': destination_place_id},
+			travelMode: travel_mode
+		}, function(response, status) {
+			if (status === google.maps.DirectionsStatus.OK) {
+				directionsDisplay.setDirections(response);
+			} else {
+				window.alert('Directions request failed due to ' + status);
+			}
+		});
+	}
+
+}
+// callback matriz de distancia 
+function callback(response, status) {
+	if (status == google.maps.DistanceMatrixStatus.OK) {
+		var origins = response.originAddresses;
+		var destinations = response.destinationAddresses;
+		var outputDiv = $('.modal-content #output');
+		var costo = $('#costo');
+		var calculo;
+
+		for (var i = 0; i < origins.length; i++) {
+			var results = response.rows[i].elements;
+			for (var j = 0; j < results.length; j++) {
+				var element = results[j];
+				var distance = element.distance.text;
+				var duration = element.duration.text;
+				var from = origins[i];
+				var to = destinations[j];
+				// ingresar datos al modal 
+				outputDiv.empty().html('travel '+ distance + ' in ' + duration);
+				// calculo costo carrera 
+				calculo = (((element.distance.value*130)/200)+200).toFixed();
+				costo.empty().html('The value of the trip is: '+'$ ' + calculo);
+			}
+		}
+		
+		// guardando valor carrera
+		guardarStorageCarrera(calculo);
+		$('#modal1').modal('open');
+	}
+}
+$(document).ready(function(){
+		// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+		$('.modal').modal();
+});
+
+function guardarStorageCarrera(a){
+	var valor = a;
+	localStorage.setItem('valorCarrera', valor);
 }
